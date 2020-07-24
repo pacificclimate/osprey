0bb9ef3903c613f6002311e6a145405030785d9f
Eric Yvorchuk <slim@pcic-0011.pcic.uvic.ca>
2020-07-24 15:38:31 -0700

    Format test_wps_parameters with black

--- tests/test_wps_parameters.py
+++ tests/test_wps_parameters.py
@@ -5,9 +5,7 @@ from wps_tools.testing import run_wps_process, local_path
 from osprey.processes.wps_parameters import Parameters
 
 
-@pytest.mark.parametrize(
-    ("config"), [local_path("sample_parameter_config.cfg")]
-)
+@pytest.mark.parametrize(("config"), [local_path("sample_parameter_config.cfg")])
 def test_parameters_local(config):
     params = f"config={config};"
     run_wps_process(Parameters(), params)
