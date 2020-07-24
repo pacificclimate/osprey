98102d1726ca12fd1a2a53b6a3f738ed852e9c27
Sangwon Lim <sangwonl@uvic.ca>
2020-07-24 14:34:46 -0700

    logging out messages

--- osprey/processes/wps_convolution.py
+++ osprey/processes/wps_convolution.py
@@ -11,17 +11,43 @@ import os
 import logging
 import json
 
-LOGGER = logging.getLogger("PYWPS")
+pywps_logger = logging.getLogger("PYWPS")
+stderr_logger = logging.getLogger(__name__)
+
+
+def log_handler(process, response, message, process_step=None, level="INFO"):
+    if process_step:
+        status_percentage = process.status_percentage_steps[process_step]
+    else:
+        status_percentage = response.status_percentage
+
+    # Log to all sources
+    pywps_logger.log(getattr(logging, level), message)
+    stderr_logger.log(getattr(logging, level), message)
+    response.update_status(message, status_percentage=status_percentage)
 
 
 class Convolution(Process):
     def __init__(self):
+        self.status_percentage_steps = {
+            "start": 0,
+            "process": 10,
+            "build_output": 95,
+            "complete": 100,
+        }
         inputs = [
             LiteralInput(
                 "config",
                 "Configuration",
                 abstract="Path to input configuration file or input dictionary",
                 data_type="string",
+            ),
+            LiteralInput(
+                "loglevel",
+                "Log Level",
+                default="INFO",
+                abstract="Logging level",
+                allowed_values=list(logging._levelToName.values()),
             ),
         ]
         outputs = [
@@ -47,6 +73,10 @@ class Convolution(Process):
         )
 
     def _handler(self, request, response):
+        loglevel = request.inputs["loglevel"][0].data
+        log_handler(
+            self, response, "Starting Process", process_step="start", level=loglevel
+        )
         config = request.inputs["config"][0].data
 
         if version == "1.1.1":
@@ -55,12 +85,33 @@ class Convolution(Process):
             else:
                 config_dict = json.loads(config)
 
+            log_handler(
+                self,
+                response,
+                "Run Flux Convolution",
+                process_step="process",
+                level=loglevel,
+            )
             convolution(config_dict)
 
         elif version == "1.1.0-1":
             config_dict = read_config(config)
+            log_handler(
+                self,
+                response,
+                "Run Flux Convolution",
+                process_step="process",
+                level=loglevel,
+            )
             convolution(config)
 
+        log_handler(
+            self,
+            response,
+            "Building final flow data output",
+            process_step="build_output",
+            level=loglevel,
+        )
         case_id = config_dict["OPTIONS"]["CASEID"]
         stop_date = config_dict["OPTIONS"]["STOP_DATE"]
         end_date = str(
@@ -72,4 +123,7 @@ class Convolution(Process):
 
         response.outputs["output"].file = os.path.join(directory, filename)
 
+        log_handler(
+            self, response, "Process Complete", process_step="complete", level=loglevel
+        )
         return response
