f5032e8c4def25ae6d13c65bcd8bd2cf8ac6bcf7
Sangwon Lim <sangwonl@uvic.ca>
2020-08-10 13:23:57 -0700

    comment out test_wps_convolution

--- tests/test_wps_convolution.py
+++ tests/test_wps_convolution.py
@@ -6,13 +6,16 @@ from pkg_resources import resource_filename
 from .common import run_wps_process
 from osprey.processes.wps_convolution import Convolution
 
-
-@mark.slow
-@mark.online
-@mark.ignore  # The test will be ignored until RVIC's internal issues are resolved
-@mark.parametrize(
-    ("config"), [f"{resource_filename(__name__, 'configs/convolve_opendap.cfg')}"],
-)
-def test_wps_convolution(config):
-    params = ("config={0};").format(config)
-    run_wps_process(Convolution(), params)
+#********************************************************************************
+#   The test is disabled due to failues caused by RVIC's internal issues:
+#   https://github.com/UW-Hydro/RVIC/issues/96
+#   https://github.com/UW-Hydro/RVIC/issues/130
+#********************************************************************************
+# @mark.slow
+# @mark.online
+# @mark.parametrize(
+#     ("config"), [f"{resource_filename(__name__, 'configs/convolve_opendap.cfg')}"],
+# )
+# def test_wps_convolution(config):
+#     params = ("config={0};").format(config)
+#     run_wps_process(Convolution(), params)
