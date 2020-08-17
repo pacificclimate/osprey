b0ed6b2e96545f1e7f70d34bb93ca90b569c39af
Sangwon Lim <sangwonl@uvic.ca>
2020-08-17 15:38:35 -0700

    params_config template

--- osprey/processes/wps_parameters.py
+++ osprey/processes/wps_parameters.py
@@ -27,6 +27,58 @@ from datetime import datetime
 
 class Parameters(Process):
     def __init__(self):
+        self.config_template = {
+            # configuration dictionary used for RVIC parameters
+            # required user inputs are defined as None value
+            "OPTIONS": {
+                "LOG_LEVEL": "INFO",
+                "VERBOSE": True,
+                "CLEAN": False,
+                "CASEID": None,
+                "GRIDID": None,
+                "CASE_DIR": None,
+                "TEMP_DIR": None,
+                "REMAP": False,
+                "AGGREGATE": False,
+                "AGG_PAD": 25,
+                "NETCDF_FORMAT": "NETCDF4",
+                "NETCDF_ZLIB": False,
+                "NETCDF_COMPLEVEL": 4,
+                "NETCDF_SIGFIGS": None,
+                "SUBSET_DAYS": None,
+                "CONSTRAIN_FRACTIONS": False,
+                "SEARCH_FOR_CHANNEL": False,
+            },
+            "POUR_POINTS": {
+                "FILE_NAME": None,
+            },
+            "UH_BOX": {
+                "FILE_NAME": None,
+            },
+            "ROUTING": {
+                "FILE_NAME": None,
+                "LONGITUDE_VAR": "lon",
+                "LATITUDE_VAR": "lat",
+                "FLOW_DISTANCE_VAR": "Flow_Distance",
+                "FLOW_DIRECTION_VAR": "Flow_Direction",
+                "BASIN_ID_VAR": "Basin_ID",
+                "VELOCITY": "velocity",
+                "DIFFUSION": "diffusion",
+                "VELOCITY": 1,
+                "DIFFUSION": 2000,
+                "OUTPUT_INTERVAL": 86400,
+                "BASIN_FLOWDAYS": 100,
+                "CELL_FLOWDAYS": 4,
+            },
+            "DOMAIN": {
+                "FILE_NAME": None,
+                "LONGITUDE_VAR": "lon"
+                "LATITUDE_VAR": "lat"
+                "LAND_MASK_VAR": "mask"
+                "FRACTION_VAR": "frac"
+                "AREA_VAR": "area"
+            },
+        }
         self.status_percentage_steps = {
             "start": 0,
             "process": 10,
