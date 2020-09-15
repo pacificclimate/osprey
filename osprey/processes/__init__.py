e516c831d5efea9b05a85db999adf78e75074586
Sangwon Lim <sangwonl@uvic.ca>
2020-09-15 10:17:30 -0700

    add wps_convert in __init__.py

--- osprey/processes/__init__.py
+++ osprey/processes/__init__.py
@@ -1,8 +1,10 @@
+from .wps_convert import Convert
 from .wps_convolution import Convolution
 from .wps_parameters import Parameters
 from .wps_full_rvic import FullRVIC
 
 processes = [
+    Convert(),
     Convolution(),
     Parameters(),
     FullRVIC(),
