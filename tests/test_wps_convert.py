2179478f44b7a35c93fe1a4ceaec56b280d7ecde
Sangwon Lim <sangwonl@uvic.ca>
2020-09-16 13:26:44 -0700

    mark online for convert_mixed.cfg test case

--- tests/test_wps_convert.py
+++ tests/test_wps_convert.py
@@ -6,11 +6,17 @@ from osprey.processes.wps_convert import Convert
 
 
 @mark.parametrize(
-    ("config"), [
-        f"{resource_filename(__name__, 'data/configs/convert_local.cfg')}",
-        f"{resource_filename(__name__, 'data/configs/convert_mixed.cfg')}",
-    ],
+    ("config"), [f"{resource_filename(__name__, 'data/configs/convert_local.cfg')}",],
 )
-def test_wps_convolution(config):
+def test_wps_convolution_local(config):
+    params = ("convert_config={0};").format(config)
+    run_wps_process(Convert(), params)
+
+
+@mark.online
+@mark.parametrize(
+    ("config"), [f"{resource_filename(__name__, 'data/configs/convert_mixed.cfg')}",],
+)
+def test_wps_convolution_https(config):
     params = ("convert_config={0};").format(config)
     run_wps_process(Convert(), params)
