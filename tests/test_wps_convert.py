f3a369d2739c339ace1bf3f945ae1f375df43828
Sangwon Lim <sangwonl@uvic.ca>
2020-09-16 13:13:37 -0700

    update pytest and jupter lab demo

--- tests/test_wps_convert.py
+++ tests/test_wps_convert.py
@@ -6,7 +6,10 @@ from osprey.processes.wps_convert import Convert
 
 
 @mark.parametrize(
-    ("config"), [f"{resource_filename(__name__, 'data/configs/convert_local.cfg')}",],
+    ("config"), [
+        f"{resource_filename(__name__, 'data/configs/convert_local.cfg')}",
+        f"{resource_filename(__name__, 'data/configs/convert_mixed.cfg')}",
+    ],
 )
 def test_wps_convolution(config):
     params = ("convert_config={0};").format(config)
