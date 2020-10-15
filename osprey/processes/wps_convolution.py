c8e4096d0698fd695929754cc3085ddf52f07071
Sangwon Lim <sangwonl@uvic.ca>
2020-10-15 11:45:07 -0700

    additional required inputs for wps_convolution

--- osprey/processes/wps_convolution.py
+++ osprey/processes/wps_convolution.py
@@ -27,7 +27,7 @@ class Convolution(Process):
                 "CASE_DIR": None,
                 "CASEID": None,
                 "CASESTR": "historical",
-                "CALENDAR": None,
+                "CALENDAR": "standard",
                 "RUN_TYPE": "drystart",  # automatic run
                 "RUN_STARTDATE": None,
                 "STOP_OPTION": "date",
@@ -75,15 +75,61 @@ class Convolution(Process):
             "complete": 100,
         }
         inputs = [
+            LiteralInput(
+                "case_id",
+                "Case ID",
+                abstract="Case ID for the RVIC process",
+                min_occurs=1,
+                max_occurs=1,
+                data_type="string",
+            ),
+            LiteralInput(
+                "start_date",
+                "Start Date",
+                abstract="Run start date (yyyy-mm-dd-hh). Only used for startup and drystart runs.",
+                min_occurs=1,
+                max_occurs=1,
+                data_type="string",
+            ),
+            LiteralInput(
+                "stop_date",
+                "Stop Date",
+                abstract="Run stop date based on STOP_OPTION",
+                min_occurs=1,
+                max_occurs=1,
+                data_type="string",
+            ),
+            ComplexInput(
+                "domain",
+                "Domain",
+                abstract="Path to CESM complaint domain file",
+                min_occurs=1,
+                max_occurs=1,
+                supported_formats=[FORMATS.NETCDF, FORMATS.DODS],
+            ),
+            ComplexInput(
+                "param_file",
+                "Parameter File",
+                abstract="Path to RVIC parameter file",
+                min_occurs=1,
+                max_occurs=1,
+                supported_formats=[FORMATS.NETCDF, FORMATS.DODS],
+            ),
+            ComplexInput(
+                "input_forcings",
+                "Input Forcings",
+                abstract="Path to land data netCDF forcings",
+                min_occurs=1,
+                max_occurs=1,
+                supported_formats=[FORMATS.NETCDF, FORMATS.DODS],
+            ),
             ComplexInput(
                 "config_file",
                 "Convolution Configuration File",
                 abstract="Path to input configuration file for Convolution process",
                 min_occurs=0,
                 max_occurs=1,
-                supported_formats=[
-                    Format("text/cfg", extension=".cfg", encoding="base64")
-                ],
+                supported_formats=[Format("text/cfg", extension=".cfg")],
             ),
             LiteralInput(
                 "config_dict",
@@ -111,8 +157,60 @@ class Convolution(Process):
             status_supported=True,
         )
 
+    def collect_args(self, request):
+        inputs = [request.inputs[k][0] for k in request.inputs.keys()]
+
+        args = {}
+        for i in inputs:
+            if vars(i)["_file"] != None:
+                args[i.identifier] = i.file
+            elif vars(i)["_url"] != None:
+                args[i.identifier] = i.url
+            else:
+                args[i.identifier] = i.data
+
+        return args
+
+    def config_hander(self, unprocessed, args):
+        """
+        This function enables users to provide dictionary-like string for Configuration input.
+        If CASE_DIR and REST_DATE are not provided from a user, their values are derived from
+        CASEID and STOP_DATE by default.
+        """
+        unprocessed = eval(unprocessed)
+        processed = self.config_template
+        try:
+            for upper_key in unprocessed.keys():
+                for lower_key in unprocessed[upper_key].keys():
+                    processed[upper_key][lower_key] = unprocessed[upper_key][lower_key]
+
+            if processed["OPTIONS"]["CASE_DIR"] == None:
+                processed["OPTIONS"]["CASE_DIR"] = os.path.join(
+                    self.workdir, processed["OPTIONS"]["CASEID"]
+                )
+            if processed["OPTIONS"]["REST_DATE"] == None:
+                processed["OPTIONS"]["REST_DATE"] = processed["OPTIONS"]["STOP_DATE"]
+
+            processed["OPTIONS"]["CASEID"] = args["case_id"]
+            processed["OPTIONS"]["RUN_STARTDATE"] = args["start_date"]
+            processed["OPTIONS"]["STOP_DATE"] = args["stop_date"]
+            processed["DOMAIN"]["FILE_NAME"] = args["domain"]
+            processed["PARAM_FILE"]["FILE_NAME"] = args["param_file"]
+            processed["INPUT_FORCINGS"]["DATL_PATH"] = "/".join(
+                args["input_forcings"].split("/")[0:-1]
+            )
+            processed["INPUT_FORCINGS"]["DATL_FILE"] = args["input_forcings"].split(
+                "/"
+            )[-1]
+
+            return processed
+
+        except KeyError:
+            raise ProcessError("Invalid config key provided")
+
     def _handler(self, request, response):
-        loglevel = request.inputs["loglevel"][0].data
+        args = self.collect_args(request)
+        loglevel = args["loglevel"]
         log_handler(
             self,
             response,
@@ -123,9 +221,13 @@ class Convolution(Process):
         )
 
         if "config_file" in request.inputs.keys():
-            unprocessed = request.inputs["config_file"][0].file
+            unprocessed = read_config(args["config_file"].file)
         elif "config_dict" in request.inputs.keys():
-            unprocessed = request.inputs["config_dict"][0].data
+            unprocessed = args["config_dict"].data
+        else:
+            unprocessed = self.config_template
+
+        config = self.config_hander(unprocessed, args)
 
         log_handler(
             self,
@@ -135,13 +237,6 @@ class Convolution(Process):
             log_level=loglevel,
             process_step="process",
         )
-
-        if os.path.isfile(unprocessed):
-            config = read_config(unprocessed)
-        else:
-            config = config_hander(
-                self.workdir, convolution.__name__, unprocessed, self.config_template
-            )
 
         convolution(config)
 
