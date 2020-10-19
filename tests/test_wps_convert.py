c14898fd3279888af3f169b9be3bd491beef29a2
Sangwon Lim <sangwonl@uvic.ca>
2020-10-19 14:44:18 -0700

    modify test_wps_convert

--- tests/test_wps_convert.py
+++ tests/test_wps_convert.py
@@ -6,17 +6,43 @@ from osprey.processes.wps_convert import Convert
 
 
 @mark.parametrize(
-    ("config"), [f"{resource_filename(__name__, 'data/configs/convert_local.cfg')}",],
+    ("uhs_files", "station_file", "domain", "config_file"),
+    [
+        (
+            f"file:///{resource_filename(__name__, '/data/samples/sample.uh_s2')}",
+            f"file:///{resource_filename(__name__, '/data/samples/station_file.txt')}",
+            f"file:///{resource_filename(__name__, '/data/samples/sample_routing_domain.nc')}",
+            f"file:///{resource_filename(__name__, 'data/configs/convert.cfg')}",
+        )
+    ],
 )
-def test_wps_convolution_local(config):
-    params = f"convert_config={config};"
+def test_wps_convolution_local(uhs_files, station_file, domain, config_file):
+    params = (
+        f"uhs_files=@xlink:href={uhs_files};"
+        f"station_file=@xlink:href={station_file};"
+        f"domain=@xlink:href={domain};"
+        f"config_file=@xlink:href={config_file};"
+    )
     run_wps_process(Convert(), params)
 
 
 @mark.online
 @mark.parametrize(
-    ("config"), [f"{resource_filename(__name__, 'data/configs/convert_mixed.cfg')}",],
+    ("uhs_files", "station_file", "domain", "config_file"),
+    [
+        (
+            f"file:///{resource_filename(__name__, '/data/samples/sample.uh_s2')}",
+            f"file:///{resource_filename(__name__, '/data/samples/station_file.txt')}",
+            "https://docker-dev03.pcic.uvic.ca/twitcher/ows/proxy/thredds/fileServer/datasets/RVIC/sample_routing_domain.nc",
+            f"file:///{resource_filename(__name__, 'data/configs/convert.cfg')}",
+        )
+    ],
 )
-def test_wps_convolution_https(config):
-    params = f"convert_config={config};"
+def test_wps_convolution_https(uhs_files, station_file, domain, config_file):
+    params = (
+        f"uhs_files=@xlink:href={uhs_files};"
+        f"station_file=@xlink:href={station_file};"
+        f"domain=@xlink:href={domain};"
+        f"config_file=@xlink:href={config_file};"
+    )
     run_wps_process(Convert(), params)
