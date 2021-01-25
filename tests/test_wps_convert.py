59a7193e2d81217d1dd9f9d66e8bf9f9af7fc975
cairosanders <cairosanders@hotmail.com>
2021-01-25 14:23:32 -0800

    Use status report to get Process err msg for test_wps_convert_rvic_err

--- tests/test_wps_convert.py
+++ tests/test_wps_convert.py
@@ -3,10 +3,31 @@ from pytest import mark
 from pkg_resources import resource_filename
 from tempfile import NamedTemporaryFile
 from rvic.core.config import read_config
+from pywps import Service
 
-from wps_tools.testing import run_wps_process, local_path, url_path
+from wps_tools.testing import run_wps_process, local_path, url_path, client_for
 from osprey.processes.wps_convert import Convert
 from .utils import process_err_test
+
+
+def RVIC_err_test(process, params):
+    client = client_for(Service(processes=[process]))
+    datainputs = params
+    resp = client.get(
+        service="wps",
+        request="Execute",
+        version="1.0.0",
+        identifier=process.identifier,
+        datainputs=datainputs,
+    )
+
+    # exception_el.text only shows the ProcessError message if loglevel is set to DEBUG
+    exception_el = resp.xpath(
+        "/wps:ExecuteResponse/wps:Status/wps:ProcessFailed/"
+        "wps:ExceptionReport/ows:Exception/ows:ExceptionText"
+    )[0]
+
+    assert "Process error" in exception_el.text
 
 
 def build_params(uhs_files, station_file, domain, config_file):
@@ -81,5 +102,7 @@ def test_wps_convert_config_err(uhs_files, station_file, domain, config_file):
     ],
 )
 def test_wps_convert_rvic_err(uhs_files, station_file, domain, config_file):
-    params = build_params(uhs_files, station_file, domain, config_file)
-    process_err_test(Convert, params)
+    params = (
+        build_params(uhs_files, station_file, domain, config_file) + f"loglevel=DEBUG"
+    )
+    RVIC_err_test(Convert(), params)
--- tests/utils.py
+++ tests/utils.py
@@ -2,7 +2,6 @@ import io
 import pytest
 from contextlib import redirect_stderr, redirect_stdout
 from wps_tools.testing import run_wps_process
-import sys
 
 
 def process_err_test(process, datainputs):
